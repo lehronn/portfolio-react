6a7577130880176c0d59a66d59a06860c14d5576
Mateusz Stomski <lehronn@gmail.com>
2018-01-12 11:31:59 +0100

    navbar color changes and remove bootstrap class

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,7 +4,7 @@ import styles from './Nav.css';
 class Nav extends Component {
   render() {
     return (
-      <nav id="navbar" className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <nav id="navbar" className="navbar fixed-top navbar-expand-lg navbar-dark ">
         <a className="navbar-brand" href="#">Stomski</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
