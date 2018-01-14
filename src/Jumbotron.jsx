8bf3a89f7872773e869cfab53c3a86922d0dc538
Mateusz Stomski <lehronn@gmail.com>
2018-01-14 20:58:08 +0100

    typewriter effect for jumbotron header.

--- src/Jumbotron.jsx
+++ src/Jumbotron.jsx
@@ -8,7 +8,7 @@ class Jumbotron extends Component {
         <div className="container jumbotron-header text-light">
           <span className="summa text-light">&sum;</span>
           <span className="summa-subtext text-light">suma wszystkich możliwości.</span>
-          <h1 className="display-4">Mateusz Stomski</h1>
+          <h1 className="display-4 line-1 typewriter">Mateusz Stomski</h1>
           <p className="lead">Cześć, jestem początkującym web developerem i mogę pomóc Ci z Twoim następnym projektem, jeśli jesteś zainteresowany współpracą zapraszam do kontaktu.</p>
         </div>
       </div>
