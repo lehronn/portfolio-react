2813a46e2c5089fe35ad055e302b1d17a81c8d55
Mateusz Stomski <lehronn@gmail.com>
2018-01-14 09:26:31 +0100

    transparent nav with scrolling and RWD nav is working

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -15,7 +15,7 @@ class Nav extends Component {
     return (
       <nav id="navbar" className="navbar fixed-top navbar-expand-lg navbar-dark ">
         <a className="navbar-brand" href="#">Stomski</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
