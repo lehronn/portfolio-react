0ef641508c964417fcb919b994f7b686be375889
Mateusz Stomski <lehronn@gmail.com>
2018-01-28 13:18:13 +0100

    RC4

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -10,9 +10,7 @@ class Hello extends Component {
               <div className="col-12 text-light">
                 Tworzenie stron internetowych interesuje mnie od dawna, przez ponad dwa lata prowadziłem sklep internetowy, a od pół roku pracuję nad tym by Webdev stał się moim zawodem. Ukończyłem <strong>półroczny kurs Kodilli</strong> traktujący o tworzeniu stron internetowych przy pomocy JavaScript w zakresie frontend i backend a teraz czas by nabrać doświadczenia i obycia z technologiami które poznałem.
                 <br/><br/>
-                <button type="button" className="btn btn-primary float-right">
-                  <i className="fa fa-file-text-o" aria-hidden="true"></i>&nbsp;&nbsp;Moje CV
-                </button>
+                <a className="text-light btn btn-primary card-link float-right" href="http://stomski.pl/others/cv.pdf"><i className="fa fa-file-text-o" aria-hidden="true"></i>&nbsp;&nbsp;Moje CV</a>
               </div>
             </div>
           </div>
