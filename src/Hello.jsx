704eadf2d8d9ecd6f42422ce2299a4e210a0befb
Mateusz Stomski <lehronn@gmail.com>
2018-01-29 12:36:26 +0100

    1.1rc add kodilla btn and rwd fixes

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -10,7 +10,8 @@ class Hello extends Component {
               <div className="col-12 text-light">
                 Tworzenie stron internetowych interesuje mnie od dawna, przez ponad dwa lata prowadziłem sklep internetowy, a od pół roku pracuję nad tym by Webdev stał się moim zawodem. Ukończyłem <strong>półroczny kurs Kodilli</strong> traktujący o tworzeniu stron internetowych przy pomocy JavaScript w zakresie frontend i backend a teraz czas by nabrać doświadczenia i obycia z technologiami które poznałem.
                 <br/><br/>
-                <a className="text-light btn btn-primary card-link float-right" href="http://stomski.pl/others/cv.pdf"><i className="fa fa-file-text-o" aria-hidden="true"></i>&nbsp;&nbsp;Moje CV</a>
+                <a className="text-light btn btn-primary card-link float-right btn-hello" target="_blank" href="http://stomski.pl/others/kodilla.jpg"><i className="fa fa-file-text-o" aria-hidden="true"></i>&nbsp;&nbsp;Zaświadczenie o ukończeniu kursu Kodilli</a>
+                <a className="text-light btn btn-primary card-link float-right btn-hello" target="_blank" href="http://stomski.pl/others/cv.pdf"><i className="fa fa-file-text-o" aria-hidden="true"></i>&nbsp;&nbsp;Moje CV</a>
               </div>
             </div>
           </div>
